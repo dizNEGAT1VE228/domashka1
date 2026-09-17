db9edaedb7a577c64a52baa63b701c0d9fcdabe0
dizNEGAT1VE <65190419+dizNEGAT1VE228@users.noreply.github.com>
2026-09-17 07:35:05 +0300

    Add files via upload

---
+++ zadanie 2.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	int mx, x, i;
+	mx = 0;
+	i = 0;
+	printf("write number \n");
+	scanf_s("%d", &x);
+	while (x != 1)
+	{
+		i += 1;
+		if (x % 2 == 0)
+		{
+			x = x / 2;
+		}
+		else
+		{
+			x = x * 3 + 1;
+		}
+		if (mx < x)
+		{
+			mx = x;
+		}
+	}
+	printf("count steps: %d\n", i);
+	printf("max num: %d", mx);
+
+
+}
+
