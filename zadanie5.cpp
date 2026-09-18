2f0fc3c11a4acd3f0d8dda760e0dbedce0b21d0b
dizNEGAT1VE <65190419+dizNEGAT1VE228@users.noreply.github.com>
2026-09-18 08:34:37 +0300

    Add files via upload

---
+++ zadanie5.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+int main()
+{
+    int i=1, x;
+    printf("write num: ");
+    scanf_s("%d", &x);
+    while (i < x) 
+    {
+        int z = i * i;
+        if (z % 10 == i or z % 100 == i or z % 1000 == i or z % 10000 == i)
+        {
+            printf("%d - %d\n", i,z);
+        }
+        i += 1;
+    }
+}
+
